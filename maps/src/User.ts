86b907add6c383561a76bc975c77e244e0868792
Grabatui <anatoliy.bibanaev@gmail.com>
2020-11-19 23:21:53 +0500

    54. Faker and user's fake data

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,3 +1,18 @@
+import faker from 'faker';
+
 class User {
-    
+    name: string;
+    location: {
+        latitude: number,
+        longitude: number,
+    };
+
+    constructor() {
+        this.name = faker.name.firstName();
+
+        this.location = {
+            latitude: parseFloat(faker.address.latitude()),
+            longitude: parseFloat(faker.address.longitude()),
+        };
+    }
 }
