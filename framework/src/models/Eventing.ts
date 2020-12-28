133934c85a05da3388a6ab66d783fa139bfaa0bb
Grabatui <anatoliy.bibanaev@gmail.com>
2020-12-28 22:23:55 +0500

    158. User's refactoring beginning

---
+++ framework/src/models/Eventing.ts
@@ -0,0 +1,26 @@
+import {User} from './User';
+
+
+type Callback = (user: User) => void;
+
+export class Eventing {
+    private events: {[key: string]: Callback[]} = {};
+
+    public on(eventName: string, callback: Callback): void {
+        if ( ! this.events.hasOwnProperty(eventName)) {
+            this.events[eventName] = [];
+        }
+
+        this.events[eventName].push(callback);
+    }
+
+    public trigger(eventName: string): void {
+        if ( ! this.events.hasOwnProperty(eventName) || this.events[eventName].length <= 0) {
+            return;
+        }
+
+        for (const eventCallback of this.events[eventName]) {
+            eventCallback.call(this, this);
+        }
+    }
+}
--- framework/src/models/User.ts
+++ framework/src/models/User.ts
@@ -1,16 +1,17 @@
-import axios, { AxiosResponse } from 'axios';
+import {Eventing} from './Eventing';
+import { Sync } from './Sync';
 
 
-interface UserProperties {
+export interface UserProperties {
     id?: number,
     name?: string,
     age?: number,
 }
 
-type Callback = (user: User) => void;
-
 export class User {
-    private events: {[key: string]: Callback[]} = {};
+    public events: Eventing = new Eventing();
+
+    public sync: Sync<UserProperties> = new Sync<UserProperties>(`http://localhost:3000`);
 
     constructor(
         private data: UserProperties
@@ -22,42 +23,5 @@ export class User {
 
     public set(updateData: UserProperties): void {
         Object.assign(this.data, updateData);
-    }
-
-    public on(eventName: string, callback: Callback): void {
-        if ( ! this.events.hasOwnProperty(eventName)) {
-            this.events[eventName] = [];
-        }
-
-        this.events[eventName].push(callback);
-    }
-
-    public trigger(eventName: string): void {
-        if ( ! this.events.hasOwnProperty(eventName) || this.events[eventName].length <= 0) {
-            return;
-        }
-
-        for (const eventCallback of this.events[eventName]) {
-            eventCallback.call(this, this);
-        }
-    }
-
-    public fetch() {
-        const id = this.get(`id`);
-
-        axios.get(`http://localhost:3000/users/${id}`)
-            .then((response: AxiosResponse): void => {
-                this.set(response.data);
-            });
-    }
-
-    public save() {
-        const id = this.get(`id`);
-
-        if (id) {
-            axios.put(`http://localhost:3000/users/${id}`, this.data);
-        } else {
-            axios.post(`http://localhost:3000/users`, this.data);
-        }
     }
 }
