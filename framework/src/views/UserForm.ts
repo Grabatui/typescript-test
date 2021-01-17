c48b0b6cd742af22ebe228dc6ed39f8b0d76f919
Grabatui <anatoliy.bibanaev@gmail.com>
2021-01-17 14:28:38 +0500

    187. User Form events

--- framework/src/views/UserForm.ts
+++ framework/src/views/UserForm.ts
@@ -3,10 +3,22 @@ export class UserForm {
         public parent: Element
     ) {}
 
+    public eventsMap(): { [key: string]: () => void } {
+        return {
+            'click:button': this.onButtonClick,
+        };
+    }
+
+    public onButtonClick(): void {
+        console.log('Button clicked');
+    }
+
     public render(): void {
         const templateElement = document.createElement(`template`);
 
         templateElement.innerHTML = this.template();
+
+        this.bindEvents(templateElement.content);
 
         this.parent.append(templateElement.content);
     }
@@ -17,7 +29,21 @@ export class UserForm {
             <h1>User Form</h1>
 
             <input />
+
+            <button>Click Me</button>
         </div>
         `;
+    }
+
+    private bindEvents(fragment: DocumentFragment): void {
+        const events = this.eventsMap();
+
+        for (const eventKey in events) {
+            const [eventName, selector] = eventKey.split(`:`);
+
+            fragment.querySelectorAll(selector).forEach((element) => {
+                element.addEventListener(eventName, events[eventKey]);
+            });
+        }
     }
 }
